1d7013d23c283f21fe7111d56d10bac6cd22c569
Victor Ngaboyamahoro <nvichack@gmail.com>
2019-05-26 22:03:12 +0200

    Initial commit

---
+++ app/src/main/java/com/example/bookin/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.bookin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.bookin.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
