df827749fd69f8cea437c834ed72224f5c26dec9
Victor Ngaboyamahoro <nvichack@gmail.com>
2019-05-27 08:51:38 +0200

    Pulled data from database and bind to respective views

--- app/src/main/java/com/example/bookin/HotelsAdapter.java
+++ app/src/main/java/com/example/bookin/HotelsAdapter.java
@@ -1,30 +1,79 @@
 package com.example.bookin;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HotelsAdapter extends RecyclerView.Adapter<HotelsAdapter.ViewHolder> {
+    private Context context;
+    private List<DataSnapshot> hotels = new ArrayList<>();
+
+    public HotelsAdapter(Context context, List<DataSnapshot> hotels) {
+        this.context = context;
+        this.hotels = hotels;
+    }
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+        return new ViewHolder(LayoutInflater.from(context).inflate(R.layout.hotel_layout, viewGroup, false));
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+    public void onBindViewHolder(@NonNull final ViewHolder viewHolder, int i) {
+        DataSnapshot hotel = hotels.get(i);
 
+        viewHolder.nameView.setText(hotel.child("name").getValue().toString());
+        viewHolder.addressView.setText(hotel.child("Address").getValue().toString());
+        viewHolder.phoneView.setText(hotel.child("Phone").getValue().toString());
+        viewHolder.emailView.setText(hotel.child("email").getValue().toString());
+
+        for (DataSnapshot service:hotel.child("services").getChildren()) {
+            TextView textView = new TextView(context);
+            textView.setText(service.child("name").getValue().toString() + ": " +service.child("Price").getValue().toString());
+            viewHolder.servicesContainer.addView(textView);
+        }
+
+        viewHolder.toggler.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (viewHolder.toggler.getText().toString().equals("Show services")){
+                    viewHolder.toggler.setText("Show less");
+                    viewHolder.servicesContainer.setVisibility(View.VISIBLE);
+                } else {
+                    viewHolder.toggler.setText("Show services");
+                    viewHolder.servicesContainer.setVisibility(View.GONE);
+                }
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return hotels.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        public ViewHolder(@NonNull View itemView) {
+        TextView nameView, addressView, phoneView, emailView, toggler;
+        LinearLayout servicesContainer;
+        private ViewHolder(@NonNull View itemView) {
             super(itemView);
+            nameView = itemView.findViewById(R.id.hotel_name_view);
+            addressView = itemView.findViewById(R.id.address_view);
+            phoneView = itemView.findViewById(R.id.phone_view);
+            emailView = itemView.findViewById(R.id.email_view);
+            toggler = itemView.findViewById(R.id.services_toggle);
+            servicesContainer = itemView.findViewById(R.id.services_holder);
         }
     }
 }
--- app/src/main/java/com/example/bookin/MainActivity.java
+++ app/src/main/java/com/example/bookin/MainActivity.java
@@ -1,15 +1,68 @@
 package com.example.bookin;
 
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
 
 import com.example.bookin.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    private DatabaseReference databaseReference;
+    private Context context;
+
+    RecyclerView hotelsRecyclerView;
+    private List<DataSnapshot> hotels = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = this;
+        firebaseAuth = FirebaseAuth.getInstance();
+        databaseReference = FirebaseDatabase.getInstance().getReference("hotels");
+        hotelsRecyclerView = findViewById(R.id.hotels_recycler_view);
+        hotelsRecyclerView.setHasFixedSize(true);
+        hotelsRecyclerView.setLayoutManager(new LinearLayoutManager(context));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (firebaseAuth.getCurrentUser() == null){
+            startActivity(new Intent(context, LoginActivity.class));
+            finish();
+        } else {
+            databaseReference.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    for (DataSnapshot snapShot:dataSnapshot.getChildren()) {
+                        hotels.add(snapShot);
+                    }
+                    HotelsAdapter adapter = new HotelsAdapter(context, hotels);
+                    hotelsRecyclerView.setAdapter(adapter);
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                    Toast.makeText(context, databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
     }
 }
