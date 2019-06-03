5cc86387da233a3d99eca4885b05bf83e9944974
Victor Ngaboyamahoro <nvichack@gmail.com>
2019-06-03 14:45:03 +0200

    Added logout

--- app/src/main/java/com/example/bookin/MainActivity.java
+++ app/src/main/java/com/example/bookin/MainActivity.java
@@ -7,6 +7,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.example.bookin.R;
@@ -27,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
     private Context context;
 
     RecyclerView hotelsRecyclerView;
+    Toolbar toolbar;
     private List<DataSnapshot> hotels = new ArrayList<>();
 
     @Override
@@ -36,6 +41,8 @@ public class MainActivity extends AppCompatActivity {
         context = this;
         firebaseAuth = FirebaseAuth.getInstance();
         databaseReference = FirebaseDatabase.getInstance().getReference("hotels");
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         hotelsRecyclerView = findViewById(R.id.hotels_recycler_view);
         hotelsRecyclerView.setHasFixedSize(true);
         hotelsRecyclerView.setLayoutManager(new LinearLayoutManager(context));
@@ -64,5 +71,24 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options_manu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.action_logout){
+            firebaseAuth.signOut();
+            startActivity(new Intent(context, LoginActivity.class));
+            finish();
+        } else if (item.getItemId() == R.id.action_reservations){
+            //
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
